b025711df54aa2edb95cbd7fe35f0a9b78b3cfe2
JaiberS <758@holbertonschool.com>
2019-05-07 22:37:14 +0000

    [Feature] Check if list is palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: the list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int *tuple, i = 0, j = 0, conf = 0;
+
+	if (head == NULL || *head == NULL)
+		return (1);
+	tuple = malloc(sizeof(tuple));
+	for (; *head != NULL; i++, *head = (*head)->next)
+	{
+		tuple[i] = (*head)->n;
+		if ((*head)->next != NULL)
+			tuple = _realloc(tuple, i + 1, i + 2);
+	}
+	i--;
+	return (conf = cpalin(tuple, i, j));
+}
+
+/**
+ * cpalin - checks for palindrome
+ * @s: the string
+ * @j: the counter in reverse
+ * @i: the counter
+ *
+ * Return: 1 is palindrome 0 is not
+ */
+
+int cpalin(int *s, int j, int i)
+{
+	int r = 0;
+
+	if (s[j] == s[i])
+	{
+		j--;
+		i++;
+		if (i > j)
+			return (1);
+		r = cpalin(s, j, i);
+	}
+	else
+		return (0);
+	return (r);
+}
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: initial pointer
+ * @old_size: initial size
+ * @new_size: new size
+ * Return: pointer to array
+ */
+int *_realloc(int *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int *clone, *relloc, *aux;
+	unsigned int i;
+
+	if (ptr != NULL)
+		clone = ptr;
+	else
+	{
+		aux = malloc(sizeof(int) * new_size);
+		return (aux);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{ free(ptr);
+		return (0); }
+	relloc = malloc(sizeof(int) * new_size);
+	if (relloc == NULL)
+		return (0);
+	for (i = 0; i < old_size; i++)
+	{
+		*(relloc + i) = clone[i];
+	}
+	*(relloc + i) = '\0';
+	free(ptr);
+	return (relloc);
+}
