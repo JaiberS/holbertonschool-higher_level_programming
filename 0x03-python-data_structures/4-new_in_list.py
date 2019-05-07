13c09e079df13ebcd3a4070a2c48af861868aec2
javb92 <mentor37_@hotmail.com>
2019-05-07 19:01:04 +0000

    [Feature] Return more than one variable and logix 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
     ln = len(my_list)
-    if idx > ln:
+    if idx >= ln:
         return my_list
     my_new_list = []
     for i in range(ln):
