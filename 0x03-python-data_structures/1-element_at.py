0c7a626df04ef77a4270e046fdfbee6c48cdbc01
javb92 <mentor37_@hotmail.com>
2019-05-07 18:36:52 +0000

    [Fixes] Return None

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return
+        return None
     ln = len(my_list)
     if idx > ln:
-        return
+        return None
     return(my_list[idx])
