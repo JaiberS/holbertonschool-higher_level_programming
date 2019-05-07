be150f7a28920047ff96bebce3f866a5904fcc81
Lucia Rodriguez <774@holbertonschool.com>
2019-05-07 16:08:55 +0000

    [Feature] prints a matri

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    k = 0
+    if matrix == [[]]:
+        print()
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i]) - 1):
+                print("{:d}".format(matrix[i][j]), end="  ")
+                k = j
+            k += 1
+            print("{:d}".format(matrix[i][k]))
