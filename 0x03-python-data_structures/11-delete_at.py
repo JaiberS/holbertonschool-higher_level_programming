5bdf8c0103597cecb5f8dc9a4bff3b5d4e103a83
javb92 <mentor37_@hotmail.com>
2019-05-07 19:11:44 +0000

    [Feature] Delete list at idx

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+import pdb
+
+
 def delete_at(my_list=[], idx=0):
+    pdb.set_trace()
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    del my_list[idx]
+    return my_list
