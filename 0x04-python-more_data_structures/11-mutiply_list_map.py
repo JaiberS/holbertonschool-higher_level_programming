b6323e8d24a2d622a58748afac4f318b6e314ec1
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-09 20:08:09 +0000

    [Feature] Multiply a dic with an specific number

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
+    if my_list is not None:
+        if my_list == []:
+            return my_list
+        new_my_list = []
+        numberlist = []
+        for i in range(len(my_list)):
+            numberlist.append(number)
+        new_my_list.append(list(map(lambda x, y: x * y, my_list, numberlist)))
+        new_my_list = new_my_list[0]
+        return new_my_list
