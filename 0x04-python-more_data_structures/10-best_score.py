62eaa97ba68db965e3c3e75d1f18997361e6df16
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-09 22:14:49 +0000

    [Fixes] Empty dic

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and len(a_dictionary) != 0:
         maxim = -99999999999
         keymax = ''
         for i, j in a_dictionary.items():
