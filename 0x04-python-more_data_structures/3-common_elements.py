009463b130f2fc1dce83a4e1e3747e3dde9e7970
JaiberS <758@holbertonschool.com>
2019-05-09 17:20:23 +0000

    [Features] return a list with different values, pep8 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2): 
+def common_elements(set_1, set_2):
     new_list = []
     for i in range(len(set_1)):
         counter = 0
--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    new_list = []
+    new_list.append(true_common(set_1, set_2))
+    new_list.append(true_common(set_2, set_1))
+    for i in range(len(new_list[1])):
+        new_list[0].append(new_list[1][i])
+    del new_list[1]
+    return new_list
+
+
+def true_common(set_1, set_2):
+    new_list = []
+    for i in range(len(set_1)):
+        counter = 0
+        for j in range(len(set_2)):
+            if list(set_1)[i] == list(set_2)[j]:
+                counter += 1
+                continue
+        if counter == 0:
+            new_list.append(list(set_1)[i])
+    return new_list
