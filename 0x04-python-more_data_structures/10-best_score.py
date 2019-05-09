c4ae0c32f214ccd602165c4b1bf6458a33d45e1d
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-09 19:55:21 +0000

    [Fixes] Changed logic for negative values

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        maxim = 0
+        maxim = -99999999999
         keymax = ''
         for i, j in a_dictionary.items():
             if maxim < j:
