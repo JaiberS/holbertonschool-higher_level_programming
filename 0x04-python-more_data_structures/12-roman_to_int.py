687b8255cf09ffd2c4db8c024b495f24913a314e
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-09 20:41:53 +0000

    [Feature] roman to dec

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
+def roman_to_int(roman_string):
+    if not roman_string.isalpha():
+        return 0
+    if roman_string is None:
+        return 0
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    previous = 0
+    for i in roman_string:
+        current = romans.get(i, 0)
+        if current <= previous or previous == 0:
+            total += current
+        else:
+            total -= current
+            total *= -1
+        previous = romans.get(i, 0)
+    return total
