876a917a82a23d60294352d941103af67db29856
JaiberS <758@holbertonschool.com>
2019-05-20 21:49:23 +0000

    [fixes] Removed assignment from iterator

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,9 @@ def safe_print_list(my_list=[], x=0):
         j = 0
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
-            j = i
+            j += 1
         print()
-        j += 1
         return j
     except IndexError:
         print()
-        j += 1
         return j
