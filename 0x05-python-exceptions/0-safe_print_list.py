db0e6aae2341475d62afbcdc15a0dc4141d5a23e
JaiberS <758@holbertonschool.com>
2019-05-20 21:47:49 +0000

    [fixes] added variable counter

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        j = 0
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
+            j = i
         print()
-        return i + 1
+        j += 1
+        return j
     except IndexError:
         print()
-        return i
+        j += 1
+        return j
