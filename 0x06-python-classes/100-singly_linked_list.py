7efc03aad83b083ba95af45e4db985b057fcf75f
JaiberS <758@holbertonschool.com>
2019-05-22 03:37:53 +0000

    [Feature] Single linked list in python

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,10 +3,11 @@ class Node:
     def __init__(self, data, next_node=None):
         if not type(data) == int:
             raise TypeError('data must be an integer')
-        if next_node is not None or not type(next_node) == Node:
+        if next_node is None or type(next_node) == Node:
+            self.__data = data
+            self.__next_node = next_node
+        else:
             raise TypeError('next_node must be a Node Object')
-        self.__data = data
-        self.__next_node = next_node
 
     @property
     def data(self):
@@ -24,14 +25,26 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if value is None or not type(value) == Node:
+        if value is not None or not type(value) == Node:
             raise TypeError('next_node must be a Node Object')
         self.next_node = value
 
 
 class SinglyLinkedList:
     def __init__(self):
-        head = Node(0, None)
+        self.head = None
 
     def sorted_insert(self, value):
-        new_node = Node(value, None)
+        self.head = Node(value, self.head)
+
+    def __str__(self):
+        lista = []
+        while self.head is not None:
+            lista.append(self.head.data)
+            self.head = self.head.next_node
+        lista = sorted(lista)
+        str0 = ''
+        for i in range(len(lista) - 1):
+            str0 = str0 + str(lista[i]) + '\n'
+        str0 = str0 + str(lista[-1])
+        return str0
