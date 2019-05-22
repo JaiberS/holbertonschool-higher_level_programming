e58630f0019d0846249dd3869fb7800dc3ad11fe
JaiberS <758@holbertonschool.com>
2019-05-22 00:58:16 +0000

    [Feature] Compare two areas of square

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if not type(size) == int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not type(value) == int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
