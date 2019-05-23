83acd806d3883c18ee8024d5a8b74f22708ef9d6
JaiberS <758@holbertonschool.com>
2019-05-23 19:20:51 +0000

    [Feature] Add 2 integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+>>> add_integer(2, 3)
+5
+
+"""
+
+
+def add_integer(a, b=98):
+    """
+    >>> c = add_integer(2, 3)
+    """
+    if type(a) is not int:
+        if type(a) is not float:
+            raise TypeError('a must be an integer')
+        a = int(a)
+    if type(b) is not int:
+        if type(b) is not float:
+            raise TypeError('b must be an integer')
+        b = int(b)
+    return a + b
