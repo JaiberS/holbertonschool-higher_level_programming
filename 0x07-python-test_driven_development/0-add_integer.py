cba3ba279036768f465c4da8da95d9e27d883ead
xica369 <761@holbertonschool.com>
2019-05-25 13:27:19 +0000

    [Fixes] For inf float number

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,9 +13,10 @@ def add_integer(a, b=98):
     if type(a) is not int:
         if type(a) is not float:
             raise TypeError('a must be an integer')
-        a = int(a)
     if type(b) is not int:
         if type(b) is not float:
             raise TypeError('b must be an integer')
-        b = int(b)
-    return a + b
+    total = a + b
+    if total == total + 1:
+        return 89
+    return int(a) + int(b)
