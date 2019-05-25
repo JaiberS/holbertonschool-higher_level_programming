6a1f0c567c1a81bb5b354162fc5c1e65fd6fde0b
xica369 <761@holbertonschool.com>
2019-05-25 14:21:00 +0000

    [Fixes] Test in the middle and only one item

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,8 +11,10 @@ class TestMaxInteger(unittest.TestCase):
         """
         # test max integer
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 4, 3, 2]), 4)
         self.assertEqual(max_integer(), None)
         self.assertEqual(max_integer([2, 3.4]), 3.4)
+        self.assertEqual(max_integer([4]), 4)
         self.assertEqual(max_integer(["hola", "amigo"]), "hola")
         self.assertRaises(TypeError, max_integer, [1, "hola"])
         self.assertRaises(TypeError, max_integer, None)
