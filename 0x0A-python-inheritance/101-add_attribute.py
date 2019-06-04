6ce49cbd6077b2ddc4847bbf8119506c6cb554c6
JaiberS <758@holbertonschool.com>
2019-06-04 20:05:11 +0000

    [Feature] Add attribute to a class

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def add_attribute(obj, attribute, value):
+    if isinstance(obj, (list, dict, str, int, float, tuple)):
+        raise TypeError('can\'t add new attribute')
+    setattr(obj, attribute, value)
