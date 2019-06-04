6ba2d8027cb701482391067b9eb7547b85dc3d17
JaiberS <758@holbertonschool.com>
2019-06-04 17:03:51 +0000

    [Feature] area of rectangle and str

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+class BaseGeometry:
+    pass
+
+    def area(self):
+        return self._Rectangle__width * self._Rectangle__height
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(name + ' must be an integer')
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
