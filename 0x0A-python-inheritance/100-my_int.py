605eb787bcdf811bdc4c031488f5106b46fbb67a
JaiberS <758@holbertonschool.com>
2019-06-04 19:15:39 +0000

    [Feature] Integer inherit class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+class MyInt(int):
+    def __init__(self, value):
+        int.__init__(self)
+        if type(value) is int:
+            self.__value = value
+
+    def __eq__(self, other):
+        return self.__value != other
+
+    def __ne__(self, other):
+        return self.__value == other
