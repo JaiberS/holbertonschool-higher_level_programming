faf30ec6f156c1a0c4c214995295ed33172ec643
JaiberS <758@holbertonschool.com>
2019-06-05 16:02:41 +0000

    [Feature] Count the number of lines on a file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
+    with open(filename, mode='r', encoding='utf-8') as texter:
+        text = texter.read()
+        cont = text.count("\n")
+        return cont
