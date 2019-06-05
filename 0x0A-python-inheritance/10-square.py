e97bf1710b0cb7d7109fe8439bb18c3f560939c6
JaiberS <758@holbertonschool.com>
2019-06-05 01:38:43 +0000

    [Fixes] fixed init subclass

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -30,10 +30,8 @@ class Rectangle(BaseGeometry):
 class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        self._Rectangle__width = size
-        self._Rectangle__height = size
 
     def area(self):
         return self.__size * self.__size
