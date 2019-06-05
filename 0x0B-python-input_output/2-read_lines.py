8c791ff29cb0c524760d86b1525068b37a5d4107
JaiberS <758@holbertonschool.com>
2019-06-05 16:18:06 +0000

    [Features] read n lines from a text file

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
+    with open(filename, 'r', encoding='utf-8') as jfile:
+        text = jfile.read()
+        count = text.count('\n')
+        if nb_lines <= 0 or nb_lines >= count:
+            print(text, end='')
+        else:
+            splitted = text.split('\n')
+            while(len(splitted ) != nb_lines):
+                splitted.pop(-1)
+            print('\n'.join(splitted))
