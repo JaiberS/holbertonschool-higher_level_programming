461a569a16953627e13d06a34ae23e3cbc15770b
JaiberS <758@holbertonschool.com>
2019-06-06 05:05:39 +0000

    [Feature] Append word after line from word

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, mode='r') as jfile:
+        totals = ""
+        while True:
+            i = jfile.readline()
+            if i == '':
+                break
+            totals += i
+            if search_string in i:
+                totals += new_string
+    with open(filename, mode='w') as jfile:
+        jfile.write(totals)
