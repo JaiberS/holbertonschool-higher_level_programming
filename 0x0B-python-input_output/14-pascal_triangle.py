8e6d44ef12d20e94e07386c88e9ce40a07ed3ffe
JaiberS <758@holbertonschool.com>
2019-06-06 04:51:22 +0000

    [Feature] Pascal triangle

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    pt = []
+    pp = []
+    pp.append(1)
+    pt.append(pp)
+    for i in range(n - 2):
+        pp = []
+        pt2 = pt[-1]
+        i = 0
+        while i < len(pt2):
+            if i != 0:
+                pp.append(pt2[i] + pt2[i-1])
+            else:
+                pp.append(1)
+            i += 1
+        pp.append(1)
+        pt.append(pp)
+    return pt
