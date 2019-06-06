17bbe9a1d411d0fc1044212e5fade09b8d6c4b8c
JaiberS <758@holbertonschool.com>
2019-06-06 01:38:59 +0000

    [Features] append to json file

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+import sys
+
+
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+try:
+    prevl = load_from_json_file(filename)
+except Exception as e:
+    prevl = []
+i = 0
+while i < len(sys.argv) - 1:
+    i += 1
+    prevl.append(sys.argv[i])
+save_to_json_file(prevl, filename)
