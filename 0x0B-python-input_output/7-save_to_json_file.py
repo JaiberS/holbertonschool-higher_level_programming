fbf615bddec390132f013851a116a2a731d3a0db
JaiberS <758@holbertonschool.com>
2019-06-06 01:08:27 +0000

    [Feature] writes obj to text file using json

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+import json
+
+
 def save_to_json_file(my_obj, filename):
+    with open(filename, mode='w') as jfile:
+        jsondata = json.dumps(my_obj)
+        jfile.write(jsondata)
