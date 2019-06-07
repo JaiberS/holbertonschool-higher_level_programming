92092c6bcc97fc06874495e33c3856adff675d33
JaiberS <758@holbertonschool.com>
2019-06-07 19:17:28 +0000

    [Feature] print rectangle at x y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,8 +78,10 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
-            print('#' * self.__width)
+            print(' ' * self.__x + '#' * self.__width)
 
     def __str__(self):
         str0 = "[Rectangle] (" + str(self.id) + ") " + str(self.__x) + "/"
