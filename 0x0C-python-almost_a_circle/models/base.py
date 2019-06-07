0b1c0fe3d64aa077164469e6c2bade018157e402
JaiberS <758@holbertonschool.com>
2019-06-07 21:25:31 +0000

    [Feature] Json work

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import json
+
+
 class Base:
     __nb_objects = 0
 
@@ -16,3 +19,32 @@ class Base:
     @nb_objects.setter
     def nb_objects(self, value):
         self.__nb_objects = value
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        js = ""
+        ls = []
+        for i in list_objs:
+            dic = i.to_dictionary()
+            ls.append(dic)
+        js += i.to_json_string(ls)
+        with open(cls.__name__ + ".json", mode='w') as f:
+            f.write(js)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,8 @@ class Square(Rectangle):
         if size <= 0:
             raise ValueError('width must be > 0')
         self.__size = size
+        self.__x = x
+        self.__y = y
 
     @property
     def size(self):
@@ -21,5 +23,41 @@ class Square(Rectangle):
             raise TypeError('width must be an integer')
         if value <= 0:
             raise ValueError('width must be > 0')
-        self.update(width=value)
         self.__size = value
+
+    def __str__(self):
+        str0 = "[" + self.__class__.__name__ + "] (" + str(self.id) + ") "
+        str1 = str(self.__x) + "/" + str(self.__y) + " - " + str(self.__size)
+        return str0 + str1
+
+    def update(self, *args, **kwargs):
+        if args is None or len(args) == 0:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.__size = kwargs['size']
+            if 'x' in kwargs:
+                self.__x = kwargs['x']
+            if 'y' in kwargs:
+                self.__y = kwargs['y']
+        else:
+            if len(args) < 1:
+                return
+            self.id = args[0]
+            if len(args) < 2:
+                return
+            self.__size = args[1]
+            if len(args) < 3:
+                return
+            self.__x = args[2]
+            if len(args) < 4:
+                return
+            self.__y = args[3]
+
+    def to_dictionary(self):
+        d = {}
+        d.setdefault('id', self.id)
+        d.setdefault('size', self.__size)
+        d.setdefault('x', self.__x)
+        d.setdefault('y', self.__y)
+        return d
