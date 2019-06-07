ec505588c90c6e645302d9fd24ca7ee51459ce17
JaiberS <758@holbertonschool.com>
2019-06-07 19:08:04 +0000

    [Feature] Base and rectangle classes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
+
+    @property
+    def nb_objects(self):
+        return self.__nb_objects
+
+    @nb_objects.setter
+    def nb_objects(self, value):
+        self.__nb_objects = value
