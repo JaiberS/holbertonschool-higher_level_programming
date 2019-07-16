d17ee6be899ec7f9e151da1a0e622db2597444a6
JaiberS <758@holbertonschool.com>
2019-07-16 21:16:17 +0000

    [Feature] Create table foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- create user and database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT, state_id INT NOT NULL, name VARCHAR(256) NOT NULL	, PRIMARY KEY(id), FOREIGN KEY(state_id) REFERENcES states(id));
