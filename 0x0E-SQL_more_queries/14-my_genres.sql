64390f98160b0af9df611b36b8aac88ed2936f31
JaiberS <758@holbertonschool.com>
2019-07-17 00:38:11 +0000

    [Fixes] Can be any id

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- Show genre from a show
-SELECT DISTINCT tv_genres.name FROM tv_genres, tv_shows, tv_show_genres WHERE tv_genres.id=tv_show_genres.genre_id and tv_show_genres.show_id = 8 ORDER BY tv_genres.name ASC;
+SELECT DISTINCT tv_genres.name FROM tv_genres, tv_shows, tv_show_genres WHERE tv_genres.id=tv_show_genres.genre_id and tv_show_genres.show_id = tv_shows.id and tv_shows.title = 'Dexter' ORDER BY tv_genres.name ASC;
