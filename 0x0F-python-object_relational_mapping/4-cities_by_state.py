64ad9482e07298bd637ffc9af419536f66219826
JaiberS <758@holbertonschool.com>
2019-08-09 19:14:27 +0000

    [Feature] select cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+
+""" Write a script that lists all states from the database hbtn_0e_0_usa: """
+db = MySQLdb.connect(passwd=argv[2], db=argv[3])
+c = db.cursor()
+c.execute("""SELECT cities.id, cities.name, states.name FROM states, cities
+        WHERE states.id = cities.state_id ORDER BY cities.id ASC""")
+items = c.fetchall()
+for i in items:
+    print(i)
