ccfef4874114403965d855c112d592bb2cc3971b
JaiberS <758@holbertonschool.com>
2019-08-09 21:35:51 +0000

    [Feature] Create columns of a table

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""State Model"""
+
+from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Model State"""
+    __tablename__ = 'states'
+    id = Column(Integer, Sequence('seq_reg_id', start=1, increment=1),
+                primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
