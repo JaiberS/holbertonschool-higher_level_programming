470065db0631d6f9ba62e8c016bdbf8a27234f66
JaiberS <758@holbertonschool.com>
2019-08-09 15:19:35 +0000

    [Feature] Show state name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+
+""" Write a script that lists all states from the database hbtn_0e_0_usa: """
+db = MySQLdb.connect(passwd=argv[2], db=argv[3])
+c = db.cursor()
+c.execute("""SELECT * FROM states WHERE name = %s ORDER BY id ASC""", (argv[4],))
+items = c.fetchall()
+for i in items:
+    print(i)
