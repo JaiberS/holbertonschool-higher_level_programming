705fb7a41822f45127a95e2e19663d4acbf920c6
JaiberS <758@holbertonschool.com>
2019-08-09 22:18:37 +0000

    [Feature] print first state or nothing if empty

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,10 +7,10 @@ from sys import argv
 
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                               .format(argv[1], argv[2], argv[3]),
-                               pool_pre_ping=True)
-        session = sessionmaker(bind=engine)()
-        result = session.query(State).order_by(State.id)
-        for i in result:
-            print (str(i.id) + ": " + i.name)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    session = sessionmaker(bind=engine)()
+    result = session.query(State).order_by(State.id)
+    for i in result:
+        print (str(i.id) + ": " + i.name)
