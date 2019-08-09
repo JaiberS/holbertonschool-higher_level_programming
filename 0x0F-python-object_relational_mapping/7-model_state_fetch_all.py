88fb87d2acafaa6364e06a9ce9f4068708395a31
JaiberS <758@holbertonschool.com>
2019-08-09 22:11:09 +0000

    [Feature] show table in python

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""State Model"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                               .format(argv[1], argv[2], argv[3]),
+                               pool_pre_ping=True)
+        session = sessionmaker(bind=engine)()
+        result = session.query(State).order_by(State.id)
+        for i in result:
+            print (str(i.id) + ": " + i.name)
