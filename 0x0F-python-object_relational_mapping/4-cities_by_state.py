5f732a94736a7b4eadf27a95bc6db3204eaa023c
javb92 <mentor37_@hotmail.com>
2019-08-11 02:49:52 +0000

    [Fixes] Requirement fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa: """
 from sys import argv
 import MySQLdb
 
 
 if __name__ == "__main__":
-    """ Write a script that lists all states from the database hbtn_0e_0_usa: """
-    db = MySQLdb.connect(passwd=argv[2], db=argv[3])
+    """ Write a script lists all states from the database hbtn_0e_0_usa: """
+    db = MySQLdb.connect(passwd=argv[2], db=argv[3], user=argv[1],
+                         host="localhost", port=3306)
     c = db.cursor()
     c.execute("""SELECT cities.id, cities.name, states.name FROM states, cities
     WHERE states.id = cities.state_id ORDER BY cities.id ASC""")
     items = c.fetchall()
     for i in items:
         print(i)
+    c.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+""" Write a script lists all states from the database hbtn_0e_0_usa: """
 from sys import argv
 import MySQLdb
 
 
 if __name__ == "__main__":
-    """ Write a script that lists all states from the database hbtn_0e_0_usa: """
+    """ Write a script lists all states from the database hbtn_0e_0_usa: """
     db = MySQLdb.connect(passwd=argv[2], db=argv[3])
     c = db.cursor()
     c.execute("""SELECT cities.name FROM states, cities
-    WHERE states.id = cities.state_id and states.name = %s 
+    WHERE states.id = cities.state_id and states.name = %s
     ORDER BY cities.id ASC""", (argv[4],))
     items = c.fetchall()
     str0 = ""
     for i in items:
         str0 = str0 + i[0] + ", "
     print(str0[0:-2])
+    c.close()
+    db.close()
