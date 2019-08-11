cb4b08b7de016dd00d07c0e9abfd2086900c2266
JaiberS <758@holbertonschool.com>
2019-08-11 20:04:35 +0000

    [Feature] Safe filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa: """
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(passwd=sys.argv[2], db=sys.argv[3], user=sys.argv[1],
+                         host="localhost", port=3306)
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WHERE name
+              LIKE BINARY %s ORDER BY id ASC
+              """, (sys.argv[4],))
+    items = c.fetchall()
+    if items:
+        for i in items:
+            print("{}".format(i))
+    c.close()
+    db.close()
