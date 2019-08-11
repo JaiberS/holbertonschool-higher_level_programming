4866b5ff505033be1eb0ab04606d0c0420a348b5
javb92 <mentor37_@hotmail.com>
2019-08-11 02:35:56 +0000

    [Fixes] Argv import

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ Write a script that lists all states from the database hbtn_0e_0_usa: """
-from sys import argv
+import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(passwd=argv[2], db=argv[3], user=argv[1],
+    db = MySQLdb.connect(passwd=sys.argv[2], db=argv[3], user=argv[1],
                          host="localhost", port=3306)
     c = db.cursor()
     c.execute("""SELECT * FROM states WHERE name
     LIKE BINARY "{}" ORDER BY id ASC"""
-              .format(argv[4])
+              .format(sys.argv[4])
     items = c.fetchall()
     for i in items:
         print("{}".format(i))
