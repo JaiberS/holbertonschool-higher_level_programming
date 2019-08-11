af87197df29cd5b2903098f67c44c80348bc1367
JaiberS <758@holbertonschool.com>
2019-08-11 19:46:34 +0000

    [Feature] print cities and states

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""State Model"""
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    MetaData().create_all(engine)
+    session = sessionmaker(bind=engine)()
+    result = session.query(State).order_by(State.id)
+    for i in result:
+        print(str(i.id) + ": " + i.name)
+        for j in i.cities:
+            print(" "*4 + str(j.id) + ": " + j.name)
