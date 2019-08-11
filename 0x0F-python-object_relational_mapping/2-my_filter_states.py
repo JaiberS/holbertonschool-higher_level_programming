ad6ecf4951b97e8577be1fef98970e93ae80c924
javb92 <mentor37_@hotmail.com>
2019-08-11 02:17:57 +0000

    [Fixes] fixed requirements

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa: """
 from sys import argv
 import MySQLdb
 
 
 if __name__ == "__main__":
-    """ Write a script that lists all states from the database hbtn_0e_0_usa: """
-    db = MySQLdb.connect(passwd=argv[2], db=argv[3])
+    """ Write a script lists all states from the database hbtn_0e_0_usa: """
+    db = MySQLdb.connect(passwd=argv[2], db=argv[3], user=argv[1],
+                         host="localhost", port=3306)
     c = db.cursor()
-    c.execute("""SELECT * FROM states WHERE name = %s ORDER BY id ASC""", (argv[4],))
+    c.execute("""SELECT * FROM states WHERE name = "{}" ORDER BY id ASC"""
+              .format(argv[4])
     items = c.fetchall()
     for i in items:
         print(i)
