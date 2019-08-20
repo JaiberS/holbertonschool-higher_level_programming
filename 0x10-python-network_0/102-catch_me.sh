2f7ac0ecab9a466cee28f0ad4882ef29652e5a25
JaiberS <758@holbertonschool.com>
2019-08-20 22:14:31 +0000

    [Fixes] Added silent mode

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # catch the catch me file
-curl -d "user_id=98" -H "Origin: HolbertonSchool" -LX PUT 0.0.0.0:5000/catch_me
+curl -sd "user_id=98" -H "Origin: HolbertonSchool" -LX PUT 0.0.0.0:5000/catch_me
