43fd49fd9104c98793defa451b73708fa30dfd5b
JaiberS <758@holbertonschool.com>
2019-08-20 22:06:58 +0000

    [Fixes] added header for content type

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -d $2 -X POST $1
+curl -sd @$2 -X POST -H "Content-Type: application/json" $1
