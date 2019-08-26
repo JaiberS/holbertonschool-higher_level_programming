a2613940875495f75a3d48fe2e58112df808d05c
JaiberS <758@holbertonschool.com>
2019-08-26 23:50:48 +0000

    [Feature] display id of my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" Write a Python script that fetches https://intranet.hbtn.io/status """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.exit()
+    q = (sys.argv[1], sys.argv[2])
+    html = requests.get('https://api.github.com/user', auth=q).json()
+    if 'id' in html:
+        try:
+            print(str(html['id']))
+        except ValueError:
+            print("Not a valid JSON")
+    else:
+        print("None")
