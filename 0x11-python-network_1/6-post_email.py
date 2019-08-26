6fad4ff998be85f0276e82f4fe61358c3d749c54
JaiberS <758@holbertonschool.com>
2019-08-26 21:43:58 +0000

    [Feature] email post

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" Write a Python script that fetches https://intranet.hbtn.io/status """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    html = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(html.text)
