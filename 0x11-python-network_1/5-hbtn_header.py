00470a77e8440f6e4a37e039a7eb4535fd61b417
JaiberS <758@holbertonschool.com>
2019-08-26 21:11:51 +0000

    [Feature] get a header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" Write a Python script that fetches https://intranet.hbtn.io/status """
+import requests
+
+
+if __name__ == "__main__":
+    html = requests.get('https://intranet.hbtn.io/status')
+    print(html.headers['X-Request-Id'])
