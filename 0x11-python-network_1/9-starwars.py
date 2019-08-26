87717272f0af8d56c40d1319f09b18089e0d25c1
JaiberS <758@holbertonschool.com>
2019-08-26 22:57:56 +0000

    [Feature] Get Star Wars API

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" Write a Python script that fetches https://intranet.hbtn.io/status """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    q = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    html = requests.get('https://swapi.co/api/people/', params={'search': q})
+    html = html.json()
+    try:
+        print("Number of results: " + str(html['count']))
+        for i in html['results']:
+            print(i['name'])
+    except ValueError:
+        print("Not a valid JSON")
