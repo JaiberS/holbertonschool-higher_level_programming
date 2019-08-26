7cfc2c56a851f3f4bfc8510cefe31fa9905a9231
JaiberS <758@holbertonschool.com>
2019-08-26 20:39:55 +0000

    [Feature] hbtn status and post an email

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type " + str(type(html)))
+        print("\t- content: " + str(html))
+        print("\t- utf8 content: " + html.decode('utf-8'))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" takes in a URL and an email, sends a POST request to the passed URL with
+the email as a parameter, and displays the body of the response
+(decoded in utf-8)
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+    request = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(request) as response:
+        html = response.read()
+        print(html)
