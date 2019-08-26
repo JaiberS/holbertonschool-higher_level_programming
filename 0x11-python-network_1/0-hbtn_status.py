94fbd8bf6f0237c15b58711880a5d74ff4acd551
JaiberS <758@holbertonschool.com>
2019-08-26 20:42:27 +0000

    [Fixes] Double points

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
-        print("\t- type " + str(type(html)))
+        print("\t- type: " + str(type(html)))
         print("\t- content: " + str(html))
         print("\t- utf8 content: " + html.decode('utf-8'))
