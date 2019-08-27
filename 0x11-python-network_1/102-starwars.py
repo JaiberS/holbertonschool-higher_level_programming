69e4694e781aec62b553b578e397adbcebfc7348
JaiberS <758@holbertonschool.com>
2019-08-27 01:14:05 +0000

    [Feature] print api from api, films from name

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""" Write a Python script that fetches https://intranet.hbtn.io/status """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    q = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    p = 1
+    parms = {'search': q, 'page': p}
+    html = requests.get('https://swapi.co/api/people/', params=parms).json()
+    try:
+        cont = html['count']
+        print("Number of results: " + str(cont))
+        for i in range(cont):
+            for j in html['results']:
+                print(j['name'])
+                for k in j['films']:
+                    films = requests.get(k).json()
+                    print('\t' + films['title'])
+            p += 1
+            parms = {'search': q, 'page': p}
+            link = 'https://swapi.co/api/people/'
+            html = requests.get(link, params=parms).json()
+    except ValueError:
+        print("Not a valid JSON")
+    except KeyError:
+        sys.exit()
