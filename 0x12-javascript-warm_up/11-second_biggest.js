d5d0a292b9296bddcbc150b3257b30583600b9ee
JaiberS <758@holbertonschool.com>
2019-09-17 21:24:46 +0000

    [Feature] Second Biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+let myVar = process.argv.slice(2)
+let i = 0
+let numb = 0
+let numb2 = myVar[0]
+let numb3 = numb2
+if (isNaN(numb2)){
+  console.log(0)
+} else if (isNaN(myVar[1])){
+  console.log(0)
+} else {
+  while (i < myVar.length){
+    numb = myVar[i]
+    if (numb > numb2){
+      numb3 = numb2
+      numb2 = numb
+    }
+    i++
+  }
+  console.log(numb3)
+}
