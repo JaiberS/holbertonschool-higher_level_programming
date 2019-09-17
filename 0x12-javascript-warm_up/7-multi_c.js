fa3bdb975347e033ec18369b077855a6cf3babda
JaiberS <758@holbertonschool.com>
2019-09-17 14:57:47 +0000

    [Feature] if while

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+let myVar = process.argv.slice(2)
+let i = 0
+const numb = parseInt(myVar[0], 10)
+if (isNaN(numb)){
+  console.log("Missing number of occurrences")
+} else {
+  while (i < numb){
+    console.log("C is fun")
+    i++
+  }
+}
