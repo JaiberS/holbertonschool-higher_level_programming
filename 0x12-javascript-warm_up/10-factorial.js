d532ca2ace6e322586226a1ba81c18668d81c40e
JaiberS <758@holbertonschool.com>
2019-09-17 21:16:17 +0000

    [Feature] factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+function fact(a, b){
+  if (a == 1){
+    return b
+  }
+  b *= a
+  return fact(--a, b)
+}
+
+let myVar = process.argv.slice(2)
+const numb = parseInt(myVar[0], 10)
+if (isNaN(numb)){
+  console.log(1)
+} else {
+  console.log(fact(numb, 1))
+}
