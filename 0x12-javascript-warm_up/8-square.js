1114f7c760af7e5cfb5128e165198251d31532c1
JaiberS <758@holbertonschool.com>
2019-09-17 15:04:04 +0000

    [Feature] Print Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+let myVar = process.argv.slice(2)
+let i = 0
+const numb = parseInt(myVar[0], 10)
+if (isNaN(numb)){
+  console.log("Missing size")
+} else {
+  while (i < numb){
+    console.log("x".repeat(numb))
+    i++
+  }
+}
