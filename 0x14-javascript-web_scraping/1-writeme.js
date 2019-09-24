a47af3575a3e47ded948d3176f1b3ff2a6d9b1ae
JaiberS <758@holbertonschool.com>
2019-09-24 22:46:33 +0000

    [Feature] Semistandard 1 fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const Args = process.argv.slice(2);
-const fs = require('fs')
+const fs = require('fs');
 fs.writeFile(Args[0], Args[1], err => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
   }
-})
+});
