3dfacad36abb32938eb3dde0384db9a48c13bef2
JaiberS <758@holbertonschool.com>
2019-09-24 03:07:47 +0000

    [Fixes] Standard fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-exports.esrever = function (list){
-  let nlist = [];
+exports.esrever = function (list) {
+  const nlist = [];
   let i = 0;
-  while (i < list.length){
+  while (i < list.length) {
     nlist.unshift(list[i]);
     i++;
   }
