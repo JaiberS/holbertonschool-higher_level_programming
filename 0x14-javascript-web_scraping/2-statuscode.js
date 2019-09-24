f532e9820b1ea81ac25cbec87d70b5c014f03b0c
JaiberS <758@holbertonschool.com>
2019-09-24 23:10:19 +0000

    [Feature] show status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+const request = require('request');
+request (args[0], function (error, response, body) {
+ if (error) {
+  console.log(error);
+ } else {
+  console.log('code: ' + response.statusCode);
+ }
+});
