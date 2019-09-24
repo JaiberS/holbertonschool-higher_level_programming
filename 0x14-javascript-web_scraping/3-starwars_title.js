a18c409e105fd9c067eb0da0b347d3c59c83e73f
JaiberS <758@holbertonschool.com>
2019-09-24 23:25:45 +0000

    [Feature] Films by if

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+const request = require('request');
+request('http://swapi.co/api/films/' + args[0], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
