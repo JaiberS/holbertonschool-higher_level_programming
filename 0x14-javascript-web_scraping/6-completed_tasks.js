ee054e615850c7474e455377fbdbfe099008a431
JaiberS <758@holbertonschool.com>
2019-09-25 01:11:36 +0000

    [Feature] Counts completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv.slice(2);
+let id = 0;
+let counter = 0;
+const mydict = {};
+request(args[0], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    body = JSON.parse(body);
+    id = body[0].userId;
+    for (let x = 0; x < body.length; x++) {
+      if (body[x].userId !== id) {
+        mydict[id.toString()] = counter.toString();
+        id = body[x].userId;
+        counter = 0;
+      }
+      if (body[x].completed === true) {
+        counter++;
+      }
+    }
+    mydict[id.toString()] = counter.toString();
+    console.log(mydict);
+  }
+});
