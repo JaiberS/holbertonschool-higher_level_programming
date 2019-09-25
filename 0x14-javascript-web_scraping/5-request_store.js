576f428bc0aee42d90089d170e9c1379dfadbcf6
JaiberS <758@holbertonschool.com>
2019-09-25 00:31:51 +0000

    [Feature] Get and write to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const Args = process.argv.slice(2);
+const fs = require('fs');
+const request = require('request');
+request(Args[0], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+      fs.writeFile(Args[1], body, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
