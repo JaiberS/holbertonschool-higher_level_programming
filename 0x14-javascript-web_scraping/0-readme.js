9095e5f925a78b557e7081d6cc0d52981db779aa
JaiberS <758@holbertonschool.com>
2019-09-25 00:44:28 +0000

    [Feature] print the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const Args = process.argv.slice(2);
+const fs = require('fs');
+fs.readFile(Args[0], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
